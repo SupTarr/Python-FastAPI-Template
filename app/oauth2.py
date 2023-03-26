dc645d58e8f96e03b01b29a9b36975b4e07429e7
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-03-26 09:20:26 +0700

    feat: use .env file instead

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas, database, models
 from fastapi import status, HTTPException, Depends
 from fastapi.security.oauth2 import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -13,9 +14,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # Algorithm
 # Expiration time
 
-SECRET_KEY = "lRPhJJBgFYl0xQjvw3gAWxHzI4Za92Rjt3I23bqy9yU="
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
